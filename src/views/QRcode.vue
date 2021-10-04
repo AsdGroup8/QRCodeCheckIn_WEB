6bba1edc08990faf03e6cf187f4b5744fe644e9a
Zizheng Xu <13643539@student.uts.edu.au>
2021-10-05 04:15:58 +0800

    qrcode

--- src/views/QRcode.vue
+++ src/views/QRcode.vue
@@ -10,24 +10,7 @@
           <div class="img">
             <img width="70%" src="https://s3.bmp.ovh/imgs/2021/10/145d63bfa02e742a.png" alt="">
           </div>
-          <div>
-            <v-img height="250" src=item.poster></v-img>
-
-            <v-card-title>{{current.title}}</v-card-title>
-
-            <v-card-text>
-
-              <div class="my-4 text-subtitle-1">year:{{current.year}}</div>
-
-              <div>length:{{current.length}}</div>
-              <div>plot:{{current.plot}}</div>
-            </v-card-text>
-            <v-card-text>
-              <v-btn depressed color="primary">
-                More
-              </v-btn>
-            </v-card-text>
-          </div>
+          
         </div>
 
       </v-card>
@@ -41,40 +24,7 @@
 export default {
   data () {
     return {
-      movies: [
-        {
-          id: '11111',
-          title: 'Avatar',
-          year: 2009,
-          length: 10,
-          plot: 'aaaaaaaaaaa',
-          poster: 'a.png',
-        },
-        {
-          id: '2222',
-          title: 'Unforgiven',
-          year: 1992,
-          length: 11,
-          plot: 'bbbbbbbbb',
-          poster: 'b.png',
-        },
-        {
-          id: '3333',
-          title: 'Leon: The Professional',
-          year: 1994,
-          length: 12,
-          plot: 'cccccccccc',
-          poster: 'c.png',
-        },
-        {
-          id: '4444',
-          title: 'The Hurt Locker',
-          year: 1994,
-          length: 13,
-          plot: 'ddddddddddddd',
-          poster: 'd.png',
-        }
-      ],
+
     }
   },
   created () {
